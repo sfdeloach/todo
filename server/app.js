589a1a21bde074718a7e4fcc5f3cd43e2c539641
Steven DeLoach <sfdeloach@gmail.com>
2023-10-23 19:10:29 -0400

    more cleanup

--- server/app.js
+++ server/app.js
@@ -2,73 +2,41 @@ const express = require('express');
 const session = require('express-session');
 const graphql_http = require('graphql-http/lib/use/express');
 const cors = require('cors');
-
 const { schema } = require('./schema');
-const { todos, roles, sessions, users } = require('./dummyData');
-const { display } = require('./views/display');
 
-const corsOptions = { origin: ['http://localhost:5173', 'http://127.0.0.1:5173'] };
-const port = 3000;
-const sessionLife = 12 * 3600000; // twelve hours
 const app = express();
+const corsOptions = { origin: ['http://localhost:5173', 'http://127.0.0.1:5173'] };
+const port = process.env.PORT || 3000;
+const mode = process.env.MODE || 'dev';
+const sessionLife = 4 * 3600000; // four hours
 
 app.use(
   session({
     cookie: { maxAge: sessionLife },
     resave: false,
     saveUninitialized: true,
-    secret: 'foo bar baz'
+    secret: process.env.SESSION_SECRET || 'foo bar baz'
   })
 );
 
-switch (process.env.MODE) {
-  case 'development':
+switch (mode) {
+  case 'dev':
+    // intended to be used with the client in dev mode
     app.use(cors(corsOptions));
     break;
-  case 'production':
+  case 'prod':
     app.use(express.static('../client/dist'));
     break;
   default:
-    throw Error('must specify environment variable MODE');
+    throw Error("process.env.MODE must be either 'dev' or 'prod'");
 }
 
 app.use('/api', graphql_http.createHandler({ schema }));
 
 app.get('/get-session', (req, res) => {
-  console.dir(req);
   return res.json({ sessionID: req.sessionID });
 });
 
-// TODO: convert to graphQL
-app.get('/session', (req, res) => {
-  console.log('req.sessionID = ' + req.sessionID);
-  const findSession = session => session._id === req.sessionID; // remove after db conversion
-  let sessionIndex = sessions.findIndex(findSession); // remove after db conversion
-  let session = sessions.find(findSession); // convert to db call
-
-  if (session) {
-    if (Date.now() < Date.parse(session.expires)) {
-      console.log(`found non-expired session ${req.sessionID}`);
-      session.loggedIn = true;
-
-      // update expiration
-      const updatedExpiration = new Date(Date.now() + sessionLife);
-      sessions[sessionIndex].expires = updatedExpiration.toISOString(); // convert to db call
-    } else {
-      console.log(`found expired session ${req.sessionID}`);
-      session.loggedIn = false;
-
-      // remove the expired record from db
-      -1 !== sessionIndex && sessions.splice(sessionIndex, 1); // convert to db call
-    }
-  } else {
-    console.log(`did not find session ${req.sessionID}`);
-    session = { loggedIn: false };
-  }
-
-  return res.json(session);
-});
-
 app.listen(port, () => {
-  console.log(`server started in ${process.env.MODE} mode, listening on port ${port}`);
+  console.log(`server started in ${mode} mode, listening on port ${port}`);
 });
